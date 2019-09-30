7b6bc37fe1df5aedbc759f7bfdc931c67705f4d8
Caldas, Marcelo (CDC/ONDIEH/NCCDPHP) (CTR) <mcq1@cdc.gov>
2019-09-30 08:59:20 -0400

    Fortifyclean

--- m2/install.bat
+++
@@ -1,3 +0,0 @@
-cmd /C mvn install:install-file -Dfile=%PROJECTROOT0%/m2/pom.xml -DpomFile=%PROJECTROOT0%/m2/pom.xml
-cmd /C mvn install:install-file -Dfile=%PROJECTROOT0%/m2/xcodebuild/pom.xml -DpomFile=%PROJECTROOT0%/m2/xcodebuild/pom.xml
-cmd /C mvn install:install-file -Dfile=%PROJECTROOT0%/m2/sca-maven-plugin/sca-maven-plugin-18.20.jar -DpomFile=%PROJECTROOT0%/m2/sca-maven-plugin/pom.xml
