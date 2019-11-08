1756726b1321d713eddc93953e456781e86595cd
mscaldas <mcq1@cdc.gov>
2019-11-08 10:18:00 -0500

    fixing ci/cd

--- src/main/java/gov/cdc/nccdphp/esurveillance/rest/model/CDCLogEntry.kt
+++ src/main/java/gov/cdc/nccdphp/esurveillance/rest/model/CDCLogEntry.kt
@@ -1,7 +1,13 @@
 package gov.cdc.nccdphp.esurveillance.rest.model
 
-val APP_NAME: String = "CSVFileDefinition"
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
-data class CDCLogEntry(val Token: String, val Url: String, val Source: String, val TargetSite: String,
-                       val ExceptionMessage: String, val StackTrace: String,
-                       val AppName: String = APP_NAME)
+val APP_NAME = "DPRP Data Submission Portal"
+val SERVICE_NAME: String = "CSVFileDefinition"
+
+@JsonPropertyOrder("appName", "source", "environment", "token", "url", "targetSite", "exceptionMessage", "stackTrace")
+data class CDCLogEntry(val environment:String, val token: String, val url: String, val targetSite: String,
+                       val exceptionMessage: String, val stackTrace: String) {
+    val appName: String = APP_NAME
+    val source: String = SERVICE_NAME
+}
